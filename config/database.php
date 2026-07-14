c5e23f7bdc50b1b59e57ad6147b3f7140a4bfe2e
JustinS68 <neta86@hotmail.com>
2026-07-14 20:30:30 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -10,7 +10,12 @@ class Database
 
     public function __construct()
     {
-        $url= getenv('MYSQL_URL') ?: getenv('DATABASE_URL');
+    $this->host = $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?? 'localhost';
+        $this->dbname = $_ENV['DB_DATABASE'] ?? $_ENV['DB_NAME'] ?? getenv('DB_DATABASE') ?? getenv('DB_NAME') ?? 'vite_gourmand';
+        $this->username = $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME') ?? 'root';
+        $this->password = $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?? '3306';
+        
+        $url= getenv('MYSQL_URL') ?: getenv('DATABASE_URL')  ?: (_ENV['MYSQL_URL'] ?? $_ENV['DATABASE_URL'] ?? null);
         
         if ($url) {
             $parts = parse_url($url);
